142475e671a676d8e1ed1b1cc06f25306a7d3e59
NIKHIL-KHAIRNAR <nikhkhairnar13@gmail.com>
2019-09-24 16:23:21 -0700

    commit 2

---
+++ Addition.py
@@ -0,0 +1,3 @@
+
+def Addition(num1, num2):
+    return num1+num2
--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import Addition
+
 class calculator:
     print("choose option : ")
     print("1. Addition")
@@ -6,6 +8,9 @@ class calculator:
     n = int(input("select option: "))
     if(n==1):
         print("Addition is selected")
+        num1 = int(input("Enter the number:"))
+        num2 = int(input("Enter the number:"))
+        obj = Addition(num1, num2)
     if(n==2):
         print("Subtraction is selected")
     if(n==3):
