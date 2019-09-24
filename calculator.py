be4cc0140c9070ae789f7a344d20fe557798fd0a
NIKHIL-KHAIRNAR <nikhkhairnar13@gmail.com>
2019-09-24 16:07:53 -0700

    commit 1

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class calculator:
+    print("choose option : ")
+    print("1. Addition")
+    print("2. Subtract")
+    print("3. Division")
+    n = int(input("select option: "))
+    if(n==1):
+        print("Addition is selected")
+    if(n==2):
+        print("Subtraction is selected")
+    if(n==3):
+        print("Division is selected")
